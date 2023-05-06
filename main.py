28c141963914969018f5dd83aa03332c57ed63a6
Pavel Barkov <barkov.pavel20@mail.ru>
2023-05-06 13:50:09 +0300

    проект для гитхаб

--- main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.write('Select your gender. 1 - male, 2 - female')
-st.selectbox('Gender',[1, 2])
-st.selectbox('Do you smoke', [0, 1])
-st.selectbox('Your cholesterol level', [1, 2, 3])
-st.selectbox('Your gluc level', [1, 2, 3])
-st.selectbox('Alco', [0, 1])
-st.selectbox('Training', [0, 1])
